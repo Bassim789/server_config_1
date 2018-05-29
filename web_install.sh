1539d4634f32a708f9aa1909dcf513d092215f1f
Bassim789 <bassim.matar@gmail.com>
2018-05-30 01:20:13 +0200

    ok

--- web_install.sh
+++ web_install.sh
@@ -2,6 +2,8 @@ app_name='flask_app'
 site_name='37.74.172.99'
 secret_key='5435gdfsgfv4665v4t'
 
+apache_log_dir_var='${APACHE_LOG_DIR}'
+
 apt-get update
 apt-get install apache2 -y
 apt-get install python3-dev -y
@@ -15,12 +17,10 @@ a2enmod wsgi
 cd /var/www/
 git clone "git://github.com/Bassim789/${app_name}.git"
 
-nano "/etc/apache2/sites-available/${app_name}.conf"
-
 cat >"/etc/apache2/sites-available/${app_name}.conf" <<EOL
 <VirtualHost *:80>
 	ServerName ${site_name}
-	ServerAdmin admin@${site_name}
+	ServerAdmin webmaster@localhost
 	WSGIScriptAlias / /var/www/${app_name}/${app_name}.wsgi
 	<Directory /var/www/${app_name}/>
 		Order allow,deny
@@ -31,13 +31,13 @@ cat >"/etc/apache2/sites-available/${app_name}.conf" <<EOL
 		Order allow,deny
 		Allow from all
 	</Directory>
-	ErrorLog ${APACHE_LOG_DIR}/error.log
+	ErrorLog ${apache_log_dir_var}/error.log
 	LogLevel warn
-	CustomLog ${APACHE_LOG_DIR}/access.log combined
+	CustomLog ${apache_log_dir_var}/access.log combined
 </VirtualHost>
 EOL
 
-cat >"/var/www/$app_name/${app_name}.wsgi"  <<EOL
+cat >"/var/www/${app_name}/${app_name}.wsgi"  <<EOL
 #!/usr/bin/python
 import sys
 import logging
