99f0156bf2cf6635e37601c9616f7f47fde5ab6a
Bassim789 <bassim.matar@gmail.com>
2018-05-30 02:19:16 +0200

    ok

--- run_web_install.sh
+++ run_web_install.sh
@@ -1,4 +1,5 @@
 server_name='vps324508.ovh.net'
 ip='37.74.172.99'
 sudo ssh-keygen -R $server_name
+ssh-keygen -R $server_name
 ssh 'root@'$server_name 'bash -s' < "$(dirname -- "$0")/web_install.sh"
--- web_install.sh
+++ web_install.sh
@@ -5,15 +5,16 @@ secret_key='5435gdfsgfv4665v4t'
 apache_log_dir_var='${APACHE_LOG_DIR}'
 
 apt-get update
-apt-get install apache2 -y
+#apt-get install apache2 -y
+apt-get install  nginx -y
 apt-get install python3-dev -y
 apt-get install python3-pip -y
-apt-get install libapache2-mod-wsgi -y
+#apt-get install libapache2-mod-wsgi -y
 pip3 install --upgrade pip setuptools -y
 pip3 install flask
 pip3 install pystache
 pip3 install uwsgi
-a2enmod wsgi 
+a2enmod wsgi
 cd /var/www/
 git clone "git://github.com/Bassim789/${app_name}.git"
 
@@ -37,20 +38,16 @@ cat >"/etc/apache2/sites-available/${app_name}.conf" <<EOL
 </VirtualHost>
 EOL
 
-cat >"/var/www/${app_name}/${app_name}.wsgi"  <<EOL
-#!/usr/bin/python
-import sys
-import logging
-logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0,"/var/www/${app_name}/")
-
-from FlaskApp import app as application
-application.secret_key = '${secret_key}'
+cat >"/var/www/${app_name}/wsgi.py"  <<EOL
+from ${app_name} import app
+if __name__ == "__main__":
+    app.run()
 EOL
 
-sudo a2ensite $app_name
-
-sudo service apache2 restart 
-
+#service apache2 restart
+#a2ensite $app_name
+#echo "reload apache"
+#systemctl reload apache2
 echo "run app"
+uwsgi --socket 0.0.0.0:8000 --protocol=http -w wsgi
 python3 $app_name/app.py
