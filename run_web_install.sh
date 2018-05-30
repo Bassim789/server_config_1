ac10d88d6049bb89a4c15ca7fb12c4c90af3d557
Bassim789 <bassim.matar@gmail.com>
2018-05-30 23:38:40 +0200

    ok

--- run_web_install.sh
+++ run_web_install.sh
@@ -1,5 +1,5 @@
 source $(dirname "$0")/config.sh
-sudo ssh-keygen -R $server_name1
+sudo ssh-keygen -R $server_name
 ssh-keygen -R $server_name
 cat "$(dirname "$0")/config.sh" "$(dirname -- "$0")/web_install.sh" > "$(dirname -- "$0")/web_install_config.sh"
 ssh 'root@'$server_name 'bash -s' < "$(dirname -- "$0")/web_install_config.sh"
