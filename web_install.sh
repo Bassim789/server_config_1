1df5ba1a0c91506254e8d779df09ac238e865137
Bassim789 <bassim.matar@gmail.com>
2018-05-30 19:36:40 +0200

    ok

--- web_install.sh
+++ web_install.sh
@@ -1,5 +1,7 @@
 app_name='flask_app'
 server_ip='37.74.172.99'
+site_name='newapp.simergie.ch'
+port='5000'
 
 apt-get update
 apt-get install nginx -y
@@ -15,9 +17,9 @@ git clone "git://github.com/Bassim789/${app_name}.git"
 cat >"/etc/nginx/sites-available/${app_name}" <<EOL
 server {
 	listen 80;
-	server_name ${server_ip};
+	server_name ${site_name};
 	location / {
-		proxy_pass http://127.0.0.1:5000;
+		proxy_pass http://127.0.0.1:${port};
 	}
 }
 EOL
@@ -32,8 +34,15 @@ rm /etc/nginx/sites-available/default
 rm /etc/nginx/sites-enabled/default
 service nginx start
 service nginx restart
+
+add-apt-repository ppa:certbot/certbot
+apt-get update
+apt-get install python-certbot-nginx -y
+systemctl reload nginx
+certbot --nginx -d ${site_name} -y
+
 cd "/var/www/${app_name}"
-gunicorn --bind 0.0.0.0:5000 wsgi:app
+gunicorn --bind "0.0.0.0:${port}" wsgi:app --reload
 #gunicorn my_app:app
 #uwsgi --socket 0.0.0.0:8000 --protocol=http -w wsgi
 #python3 $app_name/app.py
