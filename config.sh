be89172dc83236ca3647317c74f311975db126a0
Bassim <bassim.matar@gmail.com>
2018-06-23 21:36:40 +0200

    add empty config.sh

---
+++ config.sh
@@ -0,0 +1,31 @@
+
+# domain
+app_name=''
+site_name=''
+http_mode=''
+server_ip=''
+server_name=''
+port=''
+email_ssl=''
+
+# app
+git_app_repo=''
+root_password=''
+user_name=''
+user_password=''
+
+# db
+db_root_password=''
+db_name=''
+db_user=''
+db_user_password=''
+
+# config.json
+watcher=''
+captcha_site_key=''
+captcha_private_key=''
+
+# app user 1
+app_user1_name=''
+app_user1_email=''
+app_user1_password=''
