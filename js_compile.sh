2e605a0883dcaf3d835888baf4f38180f874bce5
Bassim789 <bassim.matar@gmail.com>
2018-06-23 21:42:32 +0200

    remove js_compile

--- js_compile.sh
+++
@@ -1,2 +0,0 @@
-cd "/var/www/${app_name}"
-node node/js2es5.js
--- run_js_compile.sh
+++
@@ -1,6 +0,0 @@
-source $(dirname "$0")/config.sh
-action='js_compile'
-sudo ssh-keygen -R $server_name
-ssh-keygen -R $server_name
-cat "$(dirname "$0")/config.sh" "$(dirname -- "$0")/${action}.sh" > "$(dirname -- "$0")/compiled/${action}.sh"
-ssh 'root@'$server_name 'bash -s' < "$(dirname -- "$0")/compiled/${action}.sh"
